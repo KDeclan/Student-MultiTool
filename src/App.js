f0d625aa9b7b7699d8dd50e4ed753b3135354c67
Declan <kdeclann@gmail.com>
2024-04-20 17:01:20 -0500

    Updated sidebar and Added UI effects

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div>
+    <div style={{ backgroundColor: 'FFFFFF' }}>
       <SideBar />
     </div>
   );
--- src/SideBar.js
+++ src/SideBar.js
@@ -1,13 +1,30 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { FaCalendarDay, FaCalendarWeek, FaCalendarAlt, FaCog, FaStopwatch, FaQuoteRight } from 'react-icons/fa';
+import { IoAddCircle } from 'react-icons/io5';
+import './SideBar.css';
 
 const SideBar = () => {
   return (
     <div
-      className="container-fluid d-flex align-items-center justify-content-center"
-      style={{ height: "100vh", backgroundColor: "#806565" }}
+      className="d-flex flex-column align-items-center justify-content-between vh-100"
+      style={{ backgroundColor: '#EEEEEE', boxShadow: '.2em 0 .5em rgba(0,0,0,0.3)', position: 'fixed', width: '20em', top: 0, left: 0 }}
     >
-      <h1>Wassup mane.</h1>
+      <div id="add-bttn-section" className="hover-effect" style={{ fontSize: '2rem', marginTop: '.5em' }}>
+      <p id="add-bttn"><IoAddCircle /> Add Task</p>
+      </div>
+      <div id="date-tags" style={{ fontSize: '2rem', margin: '1em 0' }}>
+        <p id="today-tag" className="hover-effect"><FaCalendarDay /> Today</p>
+        <p id="sevenDay-tag" className="hover-effect"><FaCalendarWeek /> Next 7 Days</p>
+        <p id="fullDay-tag" className="hover-effect"><FaCalendarAlt /> Full Schedule</p>
+      </div>
+      <div id="tools" style={{ fontSize: '2rem', margin: '1em 0' }}>
+        <p className="hover-effect"><FaStopwatch /> Pomodoro</p>
+        <p className="hover-effect"><FaQuoteRight size='1.5rem'/> Quote Machine</p>
+      </div>
+      <div id="settings" className="hover-effect" style={{ fontSize: '2rem', marginBottom: '.5em' }}>
+      <p><FaCog /> Settings</p>
+      </div>
     </div>
   );
 };
