775954bb541cf7057897619f45515eeb6a8049d3
Declan <kdeclann@gmail.com>
2024-04-20 00:18:57 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SideBar from "./SideBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <SideBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SideBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const SideBar = () => {
+  return (
+    <div
+      className="container-fluid d-flex align-items-center justify-content-center"
+      style={{ height: "100vh", backgroundColor: "#806565" }}
+    >
+      <h1>Wassup mane.</h1>
+    </div>
+  );
+};
+
+export default SideBar;
