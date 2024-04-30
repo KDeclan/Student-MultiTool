4a1e97339c522c65e30ee871896999e2190a1bee
Declan <kdeclann@gmail.com>
2024-04-30 15:57:30 -0500

    Updates

---
+++ precache-manifest.e7c1a66b7a248bfd062d0cffa7da4ed6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16bc42c7b120f73652773092837f95e2",
+    "url": "/Student-MultiTool/index.html"
+  },
+  {
+    "revision": "4ee3a57a4cbd1932ba29",
+    "url": "/Student-MultiTool/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "df125425b75c07ae6e12",
+    "url": "/Student-MultiTool/static/css/main.f84a9f20.chunk.css"
+  },
+  {
+    "revision": "4ee3a57a4cbd1932ba29",
+    "url": "/Student-MultiTool/static/js/2.a57f8858.chunk.js"
+  },
+  {
+    "revision": "cb156245ee93a85eed33",
+    "url": "/Student-MultiTool/static/js/3.44b60ca4.chunk.js"
+  },
+  {
+    "revision": "df125425b75c07ae6e12",
+    "url": "/Student-MultiTool/static/js/main.0ce0b150.chunk.js"
+  },
+  {
+    "revision": "dd0814f3006fbeddd3d9",
+    "url": "/Student-MultiTool/static/js/runtime~main.a07d19cf.js"
+  }
+]);
